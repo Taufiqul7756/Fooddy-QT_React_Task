6008e3f5eb4da0039997aa5b95fbc7ed731c2e10
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-01-07 00:39:08 +0600

    Footer - 80%

--- app/footer/Footer.jsx
+++ app/footer/Footer.jsx
@@ -1,14 +1,27 @@
+"use client";
 import React from "react";
 import SearchBar from "../components/Search";
 import { FaMagnifyingGlass } from "react-icons/fa6";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const Footer = () => {
+  const handleSubscribeClick = () => {
+    console.log("Subscribe Clicked");
+  };
   return (
     <div className=" bg-[#ff9c1c]">
       {/* Left */}
       <div className="p-16">
-        <div className="py-5 flex relative ">
+        <div className="py-5 mb-5 flex relative items-center">
           <SearchBar placeholder="Enter your Email" />
+          <button
+            type="button"
+            onClick={handleSubscribeClick}
+            className=" flex justify-center items-center gap-1 font-sm ml-2 p-1 px-2 bg-[#f15c11] text-white rounded-lg relative right-[125px] "
+          >
+            Subscribe
+            <FaArrowRightLong />
+          </button>
         </div>
         <div className="">
           <div>
