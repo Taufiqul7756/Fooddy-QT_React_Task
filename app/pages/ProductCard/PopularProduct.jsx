f59ce09452bb8273c2942919ff9816c1b6f97696
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-01-06 14:59:13 +0600

    Popular Products - Component

---
+++ app/pages/ProductCard/PopularProduct.jsx
@@ -0,0 +1,36 @@
+"use client";
+
+import Card from "@/app/components/Card";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const PopularProduct = ({ popularItems }) => {
+  console.log("Popular Product", popularItems);
+  return (
+    <div>
+      <div className="flex justify-between items-center">
+        <h3>Popular</h3>
+        <h3 className="text-[#ff9c1c]">AddMore</h3>
+      </div>
+      <div className="flex flex-wrap  gap-4">
+        {popularItems.map((item) => (
+          <div key={item.Id} className="justify-center items-center">
+            <img
+              alt={item.Name}
+              src={item.ImageUrl}
+              className="rounded-lg h-64 object-cover-fit sm:h-25 sm:w-25 lg:h-[230px] lg:w-[185px]"
+              width={400}
+              height={300}
+            />
+            <h5 className="mt-2 font-sm text-gray-950-900 sm:text-sm">
+              {item.Name}
+            </h5>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PopularProduct;
