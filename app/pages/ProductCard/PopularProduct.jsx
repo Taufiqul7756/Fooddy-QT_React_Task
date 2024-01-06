b1566750a7faa616eac0b4cc2c45fddab90e6466
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-01-06 23:15:53 +0600

    responsive and font design

--- app/pages/ProductCard/PopularProduct.jsx
+++ app/pages/ProductCard/PopularProduct.jsx
@@ -9,21 +9,57 @@ import "slick-carousel/slick/slick-theme.css";
 
 const PopularProduct = ({ popularItems }) => {
   console.log("Popular Product", popularItems);
+
   const settings = {
     infinite: true,
     speed: 400,
     slidesToShow: 5,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024, // Large screens
+        settings: {
+          slidesToShow: 5,
+        },
+      },
+      {
+        breakpoint: 900, // Large screens
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 700, // Large screens
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 500, // Small screens
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+  };
+  const handleAddMoreClick = () => {
+    // Your logic for handling the "AddMore" click
+    console.log("AddMore Clicked");
   };
   return (
     <div>
       <div className="flex justify-between items-center mb-2">
-        <h3 className="font-bold">Popular</h3>
-        <h3 className="text-[#ff9c1c] font-bold">AddMore</h3>
+        <h3 className="sm:font-thin md:font-semibold lg:font-bold">Popular</h3>
+        <h3
+          onClick={handleAddMoreClick}
+          className="text-[#ff9c1c] cursor-pointer sm:font-thin md:font-semibold lg:font-bold"
+        >
+          AddMore
+        </h3>
       </div>
-      <Slider {...settings}>
+      <Slider {...settings} className="gap-2 md:gap-4 lg:gap-8">
         {popularItems.map((item) => (
-          <div key={item.Id} className="justify-center items-center">
+          <div key={item.Id} className="justify-center items-center ">
             <img
               alt={item.Name}
               src={item.ImageUrl}
