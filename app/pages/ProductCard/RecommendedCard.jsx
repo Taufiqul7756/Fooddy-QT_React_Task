17695691fceaf783b86e0c6d0b09c5c15f5fe290
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-01-06 14:59:36 +0600

    Recommended products - component

---
+++ app/pages/ProductCard/RecommendedCard.jsx
@@ -0,0 +1,32 @@
+import Card from "@/app/components/Card";
+import React from "react";
+
+const RecommendedCard = ({ recommendedItems }) => {
+  console.log("RecommendedCard Product", recommendedItems);
+  return (
+    <div className="">
+      <div className="flex justify-between items-center">
+        <h3>Recommended</h3>
+        <h3 className="text-[#ff9c1c]">AddMore</h3>
+      </div>
+      <div className="flex flex-wrap gap-4">
+        {recommendedItems.map((item) => (
+          <div key={item.Id} className="justify-center items-center">
+            <img
+              alt={item.Name}
+              src={item.ImageUrl}
+              className="rounded-lg h-64 object-cover-fit sm:h-25 sm:w-25 lg:h-[230px] lg:w-[185px]"
+              width={400}
+              height={300}
+            />
+            <h5 className="mt-2 font-sm text-gray-950-900 sm:text-sm">
+              {item.Name}
+            </h5>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RecommendedCard;
