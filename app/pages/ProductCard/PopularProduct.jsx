d83f73687e41945ee97c8f5c5f7b8aeae2fa1462
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-01-07 21:30:09 +0600

    z index for popup

--- app/pages/ProductCard/PopularProduct.jsx
+++ app/pages/ProductCard/PopularProduct.jsx
@@ -88,7 +88,7 @@ const PopularProduct = ({ popularItems }) => {
         ))}
       </Slider>
       {isPopupOpen && (
-        <div className="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 flex items-center justify-center">
+        <div className="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
           <div className="bg-white p-4">
             <button
               onClick={handlePopupClose}
