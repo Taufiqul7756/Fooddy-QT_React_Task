1022ae3709d0bec02f86189f33d7e8e542d43d39
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-01-10 20:40:21 +0600

    mb-8

--- app/pages/ProductCard/page.jsx
+++ app/pages/ProductCard/page.jsx
@@ -37,7 +37,7 @@ const ProductCards = () => {
         <p>Loading...</p>
       ) : (
         <>
-          <div className="">
+          <div className="mb-8">
             <PopularProduct popularItems={popularItems} />
           </div>
           <div>
